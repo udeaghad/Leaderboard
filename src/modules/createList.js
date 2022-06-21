85c0d959bab9da2d45aabca698c776ca529f928d
udeaghad <dozie.udeagha@gmail.com>
2022-06-21 18:55:59 +0100

    install linters

--- src/modules/createList.js
+++ src/modules/createList.js
@@ -1,14 +1,9 @@
-const createList = (nameInput, scoreInput) => { 
-
+const createList = (nameInput, scoreInput) => {
   const scoresList = document.querySelector('.scores-list');
-  const scoreItem = document.createElement('div');  
+  const scoreItem = document.createElement('div');
   scoreItem.classList.add('score-item');
-  scoreItem.innerHTML = `${nameInput}: ${scoreInput}` 
-  
+  scoreItem.innerHTML = `${nameInput}: ${scoreInput}`;
   scoresList.appendChild(scoreItem);
-  
-  
-
-}
+};
 
 export default createList;
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'none',
+  mode: 'none',
   entry: './src/index.js',
   devServer: {
     static: './dist',
