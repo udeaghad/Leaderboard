70a5fc40b318e3690849933859dc7cc8ccc84c13
udeaghad <dozie.udeagha@gmail.com>
2022-06-21 18:23:17 +0100

    Create additem and createList module

--- src/index.js
+++ src/index.js
@@ -1,4 +1,31 @@
 import _ from 'lodash';
 import './style.css';
+import createList from './modules/createList';
+ class Score {
+    constructor (name, score) {
+        this.name = name;
+        this.score = score;
+    }
+ }
 
  
+
+ const nameInput = document.querySelector('#name');
+ const scoreInput = document.querySelector('#score');
+
+ const addItem = () => {
+    const newScore = new Score(nameInput.value, scoreInput.value);
+    createList(newScore.name, newScore.score)
+    nameInput.value = '';
+    scoreInput.value = '';
+     
+    //const scoresList = document.querySelectorAll('.scores-list')
+    const scoreItem = document.querySelectorAll('.score-item');
+    for (let i = 0;i < scoreItem.length; i+=1){
+        scoreItem[i].classList.toggle('bg-color')
+    }
+ }
+
+ const submitBtn = document.querySelector('#submit');
+
+ submitBtn.addEventListener('click', addItem)
---
+++ src/modules/createList.js
@@ -0,0 +1,14 @@
+const createList = (nameInput, scoreInput) => { 
+
+  const scoresList = document.querySelector('.scores-list');
+  const scoreItem = document.createElement('div');  
+  scoreItem.classList.add('score-item');
+  scoreItem.innerHTML = `${nameInput}: ${scoreInput}` 
+  
+  scoresList.appendChild(scoreItem);
+  
+  
+
+}
+
+export default createList;
