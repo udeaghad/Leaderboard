1b42359ca81563b11bb16001631953a2b13bf1e7
udeaghad <dozie.udeagha@gmail.com>
2022-06-22 18:58:14 +0100

    Create postScore function

--- src/modules/CreateGame.js
+++ src/modules/CreateGame.js
@@ -14,5 +14,30 @@ const createGame = async() => {
     return scoreResult
     } 
 
+    const id = '5xJIp455K6jXlXaihVnL'
+    const player = document.querySelector('#name');
+    const playerScore = document.querySelector('#score');
+
+    const postScore = async () => {
+        const baseUrl = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores/`
+      
+        const response = await fetch(baseUrl, {
+        method: 'POST',
+        body: JSON.stringify({
+          user: player.value,
+          score: playerScore.value,
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      })
+        player.value = '';
+        playerScore.value = '';
+        const result = await response.json();
+        return result;       
+      }
+
+      export {createGame, postScore, id};
+
     
   
