8a4cb39a938c43632141d4d90cb3b7f7ff61fddb
udeaghad <dozie.udeagha@gmail.com>
2022-06-22 19:11:39 +0100

    Correct linters errors

--- src/index.js
+++ src/index.js
@@ -9,19 +9,17 @@ const submitBtn = document.querySelector('#submit');
 
 submitBtn.addEventListener('click', postScore);
 
-const getScore = async() => { 
-const baseUrl = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores/`
+const getScore = async () => {
+  const baseUrl = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores/`;
 
-const request = new Request(baseUrl);
-const response = await fetch(request)
-const displayScores = await response.json();
-createList(displayScores.result)
-
-}
+  const request = new Request(baseUrl);
+  const response = await fetch(request);
+  const displayScores = await response.json();
+  createList(displayScores.result);
+};
 
 getScore();
 
 const refreshBtn = document.querySelector('.refresh-btn');
 
-refreshBtn.addEventListener('click', () => location.reload());
-     
+refreshBtn.addEventListener('click', () => window.location.reload());
--- src/modules/createList.js
+++ src/modules/createList.js
@@ -1,12 +1,11 @@
 const createList = (record) => {
-
-  for (let index = 0;index < record.length; index+=1){
-  const scoresList = document.querySelector('.scores-list');
-  const scoreItem = document.createElement('div');
-  scoreItem.classList.add('score-item');  
-  scoreItem.innerHTML = `${record[index]['user']}: ${record[index]['score']}`;
-  scoresList.appendChild(scoreItem);
-}
+  for (let index = 0; index < record.length; index += 1) {
+    const scoresList = document.querySelector('.scores-list');
+    const scoreItem = document.createElement('div');
+    scoreItem.classList.add('score-item');
+    scoreItem.innerHTML = `${record[index].user}: ${record[index].score}`;
+    scoresList.appendChild(scoreItem);
+  }
 };
 
 export default createList;
